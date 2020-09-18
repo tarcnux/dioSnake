d5f8cb1dc83df9051243bc7bedbed401241aa05b
sǝunN oısıɔɹɐ⊥ <tarcnux@gmail.com>
2020-09-18 02:21:10 +0000

    criar cobrinha

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 const canvas = document.querySelector('#snake');
 const context = canvas.getContext('2d');
 const box = 32;
+const snake = [];
 
-function criarCenario(){
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+
+function criarCenario() {
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16*box, 16*box);
 }
+
+function criarCobrinha() {
+    for(let i=0; i<snake.length; i++) {
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+criarCenario();
+criarCobrinha();
